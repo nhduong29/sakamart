33abddfa417241d0d7f5fae9f853458860fd87ea
Duong Nguyen <nhduong29@gmail.com>
2015-09-20 15:28:09 +0700

    jjjjj

--- js/script.js
+++ js/script.js
@@ -2,17 +2,15 @@
  * Created by nhuuduong on 7/6/15.
  */
 $(document).ready(function(){
-    //set intro-text align center of screen
-    var document_height = window.innerHeight;
-    var intro_text_height = $('.intro-text').height();
-    var padding_of_intro_text = (document_height -intro_text_height)/2;
-    $('.intro-text').css({'paddingTop' : padding_of_intro_text,'paddingBottom' :padding_of_intro_text});
-    //
-    var img_about_width = $('.img-about').width();
-    var img_me_width = $('.img-me').width();
-    var margin_of_img_me = (img_about_width - img_me_width)/2;
-    $('.img-me').css({'marginLeft': margin_of_img_me});
+    $("[rel='tooltip']").tooltip();
 
-    //
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').show(); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    );
 });
 
